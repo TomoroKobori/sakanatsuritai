13912a7159a192e7182fd3f09c753612577f1411
kobori <46224993+TomoroKobori@users.noreply.github.com>
2021-03-10 09:29:23 +0900

    index.tsを修正

--- myapp/src/routes/index.ts
+++ myapp/src/routes/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(_req: any, res: any, _next: any) {
+router.get('/', (req, res, next) => {
   res.render('index');
 });
 
