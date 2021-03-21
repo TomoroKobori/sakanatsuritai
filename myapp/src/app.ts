1614c2d893052b504c7975121da7ec2f0468af18
kobori <46224993+TomoroKobori@users.noreply.github.com>
2021-03-21 18:20:27 +0900

    ルートへのリクエストが通るようにする

--- myapp/src/app.ts
+++ myapp/src/app.ts
@@ -1,17 +1,21 @@
-const createError = require('http-errors');
+
 import express from "express";
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const expressLayouts = require('express-ejs-layouts');
-const sassMiddleware = require('node-sass-middleware');
 
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
+import compression from "compression";  // compresses requests
+import session from "express-session";
+import lusca from "lusca";
+import flash from "express-flash";
+import path from "path";
+import passport from "passport";
+import expressLayouts from "express-ejs-layouts";
+import methodOverride from "method-override";
+import { SESSION_SECRET } from "./util/secrets";
 
-const methodOverride = require("method-override");
-const indexRouter = require('./routes/index');
+console.log(SESSION_SECRET)
+
+// Controllers (route handlers)
+import * as homeController from "./controllers/home";
+// const indexRouter = require('./routes/index');
 const fishingSpotsRouter = require('./routes/fishingSpots')
 const fishesRouter = require('./routes/fishes')
 const userRouter = require('./routes/users')
@@ -20,53 +24,47 @@ import * as passportConfig from "./config/passport";
 
 const app = express();
 
-app.use(methodOverride("_method"));
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "../views"));
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
-
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false,
-  sourceMap: true
-}));
-
-app.use(logger('dev'));
+app.use(methodOverride("_method"));
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// 認証設定(passport)
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: SESSION_SECRET!
+}));
 app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(lusca.xframe("SAMEORIGIN"));
+app.use(lusca.xssProtection(true));
 app.use((req, res, next) => {
     res.locals.user = req.user;
     next();
 });
+app.use((req, res, next) => {
+  // After successful login, redirect back to the intended page
+  if (!req.user &&
+  req.path !== "/login" &&
+  req.path !== "/signup") {
+    req.session.returnTo = req.path;
+  }
+  next();
+});
 
+app.use(
+  express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+);
 
-app.use('/', indexRouter);
+// routes
+app.get("/", homeController.index);
+// app.use('/', indexRouter);
 app.use('/fishingSpots', fishingSpotsRouter);
 app.use('/fishes', fishesRouter);
 app.use('/users', userRouter);
 
-// catch 404 and forward to error handler
-app.use((req, res, next): void => {
-  next(createError(404));
-});
-
-// error handler
-app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction): void => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(500);
-  res.render('error');
-});
-
-module.exports = app;
+export default app;
