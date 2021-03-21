2a0076ab457f5cfdbc1ba21985d366e35a1b4ac6
kobori <46224993+TomoroKobori@users.noreply.github.com>
2021-03-21 15:30:06 +0900

    utilファイルを追加

---
+++ myapp/src/util/secrets.ts
@@ -0,0 +1,15 @@
+import logger from "./logger";
+import dotenv from "dotenv";
+
+logger.debug("Using .env file to supply config environment variables");
+dotenv.config({ path: ".env" });
+
+export const ENVIRONMENT = process.env.NODE_ENV;
+const prod = ENVIRONMENT === "production";
+
+export const SESSION_SECRET = process.env["SESSION_SECRET"];
+
+if (!SESSION_SECRET) {
+  logger.error("No client secret. Set SESSION_SECRET environment variable.");
+  process.exit(1);
+}
