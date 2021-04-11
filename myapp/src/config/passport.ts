2eb03afe3436a96102a3e04bcf327096b5c07c4b
kobori <46224993+TomoroKobori@users.noreply.github.com>
2021-04-11 11:05:42 +0900

    ログインを動的にする

--- myapp/src/config/passport.ts
+++ myapp/src/config/passport.ts
@@ -1,7 +1,10 @@
 import passport from "passport";
 import passportLocal from "passport-local";
+const bcrypt = require('bcrypt-nodejs');
 import { Request, Response, NextFunction } from "express";
+import { PrismaClient } from '@prisma/client';
 const LocalStrategy = passportLocal.Strategy;
+const prisma = new PrismaClient();
 
 // 認証後に暗号化
 passport.serializeUser((user, done) => {
@@ -14,11 +17,15 @@ passport.deserializeUser((user, done) => {
 });
 
 passport.use(new LocalStrategy({ usernameField: "email" },
-  (username: string, password: string, done: any) => {
-    if (username !== 'kobori') {
-      // error
+  async (username: string, password: string, done: any) => {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: username,
+      },
+    });
+    if (!user) {
       return done(null, false);
-    } else if (password !== 'tomoro') {
+    } else if (!bcrypt.compareSync(password, user.password)) {
       // error
       return done(null, false);
     } else {
